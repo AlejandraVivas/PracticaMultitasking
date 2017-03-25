87cb1f4aa2a58dd2e41908f1a57998800910ef97
petergutierrez <ie699398@iteso.mx>
2017-03-25 17:45:17 -0600

    Refresh de la funcion I2CRECIEVE

--- queues.c
+++ queues.c
@@ -15,16 +15,42 @@
 #include "queue.h"
 
 #include "task.h"
-#include "queue.h"
 #include "timers.h"
 #include "queues.h"
+#include "DataTypeDefinitions.h"
 
 QueueHandle_t I2C_queue_handler; // Declare the queue
 
 I2C_stuctData_t I2C_TransferData;
 
-void queue_receiver(void *pvParameters) {
 
-	I2C_queue_handler = xQueueCreate(1, sizeof(I2C_TransferData)); // Creation of the queue
+/*Function in charge of reading the I2C queue*/
+void I2Cqueue_receiver(void *pvParameters) {
+
+	I2C_stuctData_t *NewI2C_data_pointer = 0x00;
+	UBaseType_t uxNumOfItems;
+
+	I2C_queue_handler = xQueueCreate(1 , sizeof( I2C_TransferData) ); // Creation of the queue
+
+	uxNumOfItems = uxQueueMessagesWaiting (I2C_queue_handler);
+
+	if (uxNumOfItems != FALSE)
+	{
+		xQueueReceive(I2C_queue_handler, &NewI2C_data_pointer, NULL);
+	}
+}
+
+void TeraTermqueue_reciever(void *pvParameters) {
 
 }
+
+void queue_reciever(void *pvParameters) {
+
+}
+
+
+//uxQueueMessagesWaiting()
+
+
+//xQueueSend(I2C_queue_handler, I2C_pointer, NULL);
+//xQueueReceive()
